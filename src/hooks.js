275fdd2ddafb63ff366dc5e479393b9949439571
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-12-07 14:12:39 -0600

    set up hook for flipping cardss

---
+++ src/hooks.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function useFlip() {
+    const [isFacingUp, setIsFacingUp] = useState(true);
+    const flipCard = () => {
+        setIsFacingUp(isUp => !isUp);
+      };
+
+    return [isFacingUp, flipCard];
+}
+
+export default useFlip;
