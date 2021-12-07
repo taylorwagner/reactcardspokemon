68aaf2f2126b362111bb00d1eb5486885d9cd3fc
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-12-07 14:20:44 -0600

    useFlip

--- src/PlayingCard.js
+++ src/PlayingCard.js
@@ -1,14 +1,12 @@
 import React, { useState } from "react";
 import backOfCard from "./back.png";
-import useFlip from "./hooks";
+import { useFlip } from "./hooks";
 import "./PlayingCard.css"
 
 /* Renders a single playing card. */
 function PlayingCard({ front, back = backOfCard }) {
-  const [isFacingUp, setIsFacingUp] = useState(true);
-  const flipCard = () => {
-    setIsFacingUp(isUp => !isUp);
-  };
+  const [isFacingUp, flipCard] = useFlip();
+  
   return (
     <img
       src={isFacingUp ? front : back}
