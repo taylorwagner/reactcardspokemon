2fccc1b937c514a988cd36ac82cde595da79d8c0
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-12-07 21:12:18 -0600

    delete/clear

--- src/PlayingCardList.js
+++ src/PlayingCardList.js
@@ -9,14 +9,14 @@ const API_BASE_URL = "https://deckofcardsapi.com/api/deck/new/draw/";
 /* Renders a list of playing cards.
  * Can also add a new card at random. */
 function CardTable() {
-  const [cards, addCard, clear] = useAxios("cards", API_BASE_URL);
+  const [cards, addCard, clearCards] = useAxios("cards", API_BASE_URL);
 
   return (
     <div className="PlayingCardList">
       <h3>Pick a card, any card!</h3>
       <div>
         <button onClick={() => addCard(formatCard)}>Add a playing card!</button>
-        <button onClick={clear}>Clear</button>
+        <button onClick={clearCards}>Clear</button>
       </div>
       <div className="PlayingCardList-card-area">
         {cards.map(cardData => (
